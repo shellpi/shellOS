c5eb6d99e677da4312689a5b94587332ef118d99
SOMEONE360 <61124185+SOMEONE360@users.noreply.github.com>
2021-01-10 16:43:02 +0000

    Create kernel settings module (OS/kernel/settings.h)

---
+++ OS/kernel/settings.h
@@ -0,0 +1,8 @@
+#ifndef __SHELLOS_KERNEL_SETTINGS_H__
+#define __SHELLOS_KERNEL_SETTINGS_H__
+
+/* raspberry pi board, by default is 4 */
+#define RASPI 4
+
+
+#endif // ! __SHELLOS_KERNEL_SETTINGS_H__
