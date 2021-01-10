cc7db45bd57af3e3d572a576b57a44e665704bf5
SOMEONE360 <61124185+SOMEONE360@users.noreply.github.com>
2021-01-10 17:20:54 +0000

    Create kernel main file (OS/kernel/kernel.c)

---
+++ OS/kernel/kernel.c
@@ -0,0 +1,24 @@
+/******************************************
+ * file: kernel.c
+ * description: shellOS kernel main file
+ * include: UART.o, MMIO.o, utils.o
+ * output: kernel.o
+ ******************************************/
+
+#include <stdint.h>
+#include "settings.h"
+
+
+#if defined(__cplusplus)
+extern "C"
+#endif
+
+#ifdef AARCH64
+void shell_kernel(uint64_t dtb_ptr32, uint64_t x1, uint64_t x2, uint64_t x3)
+#else
+void shell_kernel(uint32_t r0, uint32_t r1, uint32_t atags)
+#endif
+{
+	main();
+	return;
+}
